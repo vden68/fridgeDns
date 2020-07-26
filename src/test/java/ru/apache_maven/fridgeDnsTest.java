4d00d09c61d212207a8c645225d3b0ead3bde21d
vden <desc@mail.ru>
2020-07-26 14:39:53 +0300

    create TestBase

--- src/test/java/ru/apache_maven/fridgeDnsTest.java
+++ src/test/java/ru/apache_maven/fridgeDnsTest.java
@@ -1,27 +1,9 @@
 package ru.apache_maven;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.open;
-
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
-public class fridgeDnsTest {
-
-	@BeforeClass
-	public void setUp() throws Exception {
-		open("https://www.dns-shop.ru/");
-		$("span.compare-link__lbl").click();
-		openMainPage();
-	}
-
-	@AfterClass
-	public void tearDown() throws Exception {
-	}
+public class fridgeDnsTest extends TestBase{
 
 	@Test
 	public void test() {
@@ -35,22 +17,5 @@ public class fridgeDnsTest {
 		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public Integer getFirstPrice() {
-		String value2 = $(".product-price__current").getAttribute("innerText").replace(" ", "");
-		Integer i2 = Integer.valueOf(value2);
-		return i2;
-	}
-
-	public void openCatalogRefrigeratorsPage() {
-		$$("a.ui-link.menu-desktop__root-title").findBy(text("Бытовая техника")).click();
-		$$("span.subcategory__title").findBy(text("Товары для кухни")).click();
-		$$("span.subcategory__title").findBy(text("Холодильное оборудование")).click();
-		$$("span.subcategory__title").findBy(text("Холодильники")).click();
-	}
-
-	public void openMainPage() {
-		$("a#header-logo.logo").click();
 	}
 }
