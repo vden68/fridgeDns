b9e698ca93d72311fc6d38f0cd1fe66a5c1c1613
vden <desc@mail.ru>
2020-07-26 20:40:29 +0300

    finish

--- src/test/java/ru/apache_maven/fridgeDnsTest.java
+++ src/test/java/ru/apache_maven/fridgeDnsTest.java
@@ -6,16 +6,20 @@ import org.testng.annotations.Test;
 public class fridgeDnsTest extends TestBase{
 
 	@Test
-	public void test() {
+	public void fridgeMinimumPriceTest() {
 		app.mainPage.openMainPage();
-		app.catalog.openCatalogRefrigeratorsPage();
+		app.catalog.openRefrigerators();
+		app.catalog.sort("По возрастанию цены");
 		Integer firstPrise = app.catalog.getFirstPrice();
 		assert(10000 > firstPrise);
-		try {
-			Thread.sleep(10000);
-		}
-		catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+	}
+	
+	@Test
+	public void fridgeMaximumPriceTest() {
+		app.mainPage.openMainPage();
+		app.catalog.openRefrigerators();
+		app.catalog.sort("По убыванию цены");
+		Integer firstPrise = app.catalog.getFirstPrice();
+		assert(100000 < firstPrise);
 	}
 }
--- src/test/java/ru/example/fw/Catalog.java
+++ src/test/java/ru/example/fw/Catalog.java
@@ -16,11 +16,26 @@ public class Catalog extends HelperBase{
 		return firstPriseInt;
 	}
 
-	public void openCatalogRefrigeratorsPage() {
+	public void openRefrigerators() {
 		$$("a.ui-link.menu-desktop__root-title").findBy(text("Бытовая техника")).click();
 	    $$("span.subcategory__title").findBy(text("Товары для кухни")).click();
 	    $$("span.subcategory__title").findBy(text("Холодильное оборудование")).click();
 	    $$("span.subcategory__title").findBy(text("Холодильники")).click();
+	}
+
+	public void sort(String typeSort) {
+		String typeSortDns = $(".top-filter__selected").getAttribute("innerText");
+		if (!typeSort.toLowerCase().equals(typeSortDns)) {
+			$(".top-filter__selected").click();
+			$$("span.ui-radio__content").findBy(text(typeSort)).click();
+			try {
+				Thread.sleep(1000);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
