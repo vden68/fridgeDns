cd5ddfa69ac77d09ba9655adf536f1edd9855071
vden <desc@mail.ru>
2020-07-26 13:44:40 +0300

    TestNG

--- src/test/java/ru/apache_maven/fridgeDnsTest.java
+++ src/test/java/ru/apache_maven/fridgeDnsTest.java
@@ -5,37 +5,52 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 
 public class fridgeDnsTest {
 
-	@Before
+	@BeforeClass
 	public void setUp() throws Exception {
 		open("https://www.dns-shop.ru/");
-        $("span.compare-link__lbl").click();
-        $("a#header-logo.logo").click();
+		$("span.compare-link__lbl").click();
+		openMainPage();
 	}
 
-	@After
+	@AfterClass
 	public void tearDown() throws Exception {
 	}
 
 	@Test
 	public void test() {
-		$$("a.ui-link.menu-desktop__root-title").findBy(text("Бытовая техника")).click();
-        $$("span.subcategory__title").findBy(text("Товары для кухни")).click();
-        $$("span.subcategory__title").findBy(text("Холодильное оборудование")).click();
-        $$("span.subcategory__title").findBy(text("Холодильники")).click();
-        String value2 = $(".product-price__current").getAttribute("innerText").replace(" ", "");
-        Integer i2 = Integer.valueOf(value2);       
-        assert(10000 > i2);
-        try {
+		openMainPage();
+		openCatalogRefrigeratorsPage();
+		Integer i2 = getFirstPrice();
+		assert(10000 > i2);
+		try {
 			Thread.sleep(10000);
-		} catch (InterruptedException e) {
+		}
+		catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public Integer getFirstPrice() {
+		String value2 = $(".product-price__current").getAttribute("innerText").replace(" ", "");
+		Integer i2 = Integer.valueOf(value2);
+		return i2;
+	}
+
+	public void openCatalogRefrigeratorsPage() {
+		$$("a.ui-link.menu-desktop__root-title").findBy(text("Бытовая техника")).click();
+		$$("span.subcategory__title").findBy(text("Товары для кухни")).click();
+		$$("span.subcategory__title").findBy(text("Холодильное оборудование")).click();
+		$$("span.subcategory__title").findBy(text("Холодильники")).click();
+	}
+
+	public void openMainPage() {
+		$("a#header-logo.logo").click();
+	}
 }
